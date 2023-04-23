d4f17cae5db33eb1e7a217618dc72c313d75e1eb
Nick VerMulm <37892064+marcello360@users.noreply.github.com>
2023-04-23 16:49:58 -0500

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="index.html">Home</a></li>
-		<li><a href="Resume.php">Resume</a></li>
-		<li><a href="CIS_Syllabi.php">Degree info</a></li>
-		<li><a href="Projects.php">Projects</a></li>
-	</ul>
-</nav>
